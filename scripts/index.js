60570bcacc08b03ea305fde11369f0a90163b285
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-11-23 15:35:23 -0500

    Issue with form validation resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,61 @@
-function handleContinue() {
+$(document).ready(function(){ //waits until document is fully loaded
 
-  let passedArray = [];
+  $("#form").submit(function(e){ //is executed everytime the submit button is clicked to submit the form
 
-  const projectName = document.getElementById("projectName");
-  const projectDescription = document.getElementById("projectDescription");
+    console.log("Continue button clicked! \nName and description are filled out.");
 
-  if (projectName.value === "") {
-    alert("The project name field is empty and must be filled out");
-  }
-  else if (projectDescription.value === "") {
-    alert("The project description field is empty and must be filled out");
-  }
-  else {
 
-    passedArray[0] = projectName.value;
-    passedArray[1] = projectDescription.value;
+    //checks to see if at-least one checkbox is selected
+    if(!$("input").is(' :checked')){
+      e.preventDefault();
 
-    const checkboxes = document.getElementsByName("check");
-
-    let counter = 2;
-
-    for (let i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) {
-        passedArray[counter] = checkboxes[i].id
-        counter++;
-      }
-    }
-    if(passedArray.length === 2) {
       alert("You must have at least one checkbox checked");
+      console.log("Error: User didn't select any checkboxes.");
+
     }
-    else {
-      sessionStorage.setItem(matrixStorage, passedArray);
-      window.location.href = "../index1.html";
+    else{ //if at-least one checkbox is selected
+      console.log("inside function that handles continue button being clicked");
+
+      console.log("All validations passed.");
+
+      //create array to store information that will be passed to the next page
+      let passedArray = [];
+
+      //have already passed validation check so they are not empty
+      const projectName = document.getElementById("projectName").value;
+      const projectDescription = document.getElementById("projectDescription").value;
+
+
+      console.log("Putting project name and description into array.");
+      //set the first two indexes of the array to project name and project description
+      passedArray[0] = projectName;
+      passedArray[1] = projectDescription;
+
+     
+      console.log("Project name inputted (passedArray[0]): " + passedArray[0]);
+      console.log("Project description inputted(passedArray[0]): " + passedArray[1]);
+
+      const checkboxes = document.getElementsByName("check"); //get all the ckecbox inputs
+
+      let counter = 2; //helps put checkbox id's in the right position in the array
+
+      for (let i = 0; i < checkboxes.length; i++) {
+        //putting the id of checkboxes selected by the user into the passedArray
+        if (checkboxes[i].checked) {
+  
+          passedArray[counter] = checkboxes[i].id;
+
+          console.log("Putting the ckeckbox id (passedArray[" + counter + "]):  " + passedArray[counter] + " into the passedArray");
+
+          counter++;
+        }
+      }
+      
     }
-  }
-}
+
+    //send array to next page via session storage/cookies/?
+
+  });
 
 
-
-window.addEventListener('load', function() {
-  let continueBtn = document.getElementById("continueButton");
-  continueBtn.addEventListener('click', handleContinue);
 });
