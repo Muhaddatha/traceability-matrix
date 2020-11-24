82b27f165362f963dc9ceb1082fc311f444ac787
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-11-24 11:04:42 -0500

    Will append input and label elements dynamically depending on user option

--- scripts/index1.js
+++ scripts/index1.js
@@ -11,13 +11,45 @@ $(document).ready(function(){
    
 
     //addding project name and project description to table
-    document.getElementById("project-name-heading").innerText += array[0];
-    document.getElementById("project-description-heading").innerText += array[1];
+    // document.getElementById("project-name-heading").innerText += array[0];
+    // document.getElementById("project-description-heading").innerText += array[1];
 
-    //hide all form fields until we know which ones the user selected
-    //document.getElementById("form-labels-and-input-fields");
+    // //hide all form fields until we know which ones the user selected
+    // formDiv = document.getElementById("form-labels-and-input-fields");
+
+    // const formLabels =  document.getElementById("form-labels-and-input-fields").getElementsByTagName("label");
+
+    // for(let i = 0; i < formLabels.length; i++){
+    //     formLabels[i].setAttribute('hidden', true);
+    // }
+
+    // const formInputFields = document.getElementById("form-labels-and-input-fields").getElementsByTagName("input");
+
+    // for(let i = 0; i < formInputFields.length; i++){
+    //     formInputFields[i].setAttribute('hidden', true);
+    // }
+
+    // const formSelectFields = document.getElementById("form-labels-and-input-fields").getElementsByTagName("select");
+
+    // for(let i = 0; i < formSelectFields.length; i++){
+    //     formSelectFields[i].setAttribute('hidden', true);
+    // }
+
+    // const commentSection = document.getElementById("form-labels-and-input-fields").getElementsById("textarea");
+    // commentSection.setAttribute('hidden', true); //fix -> hide comment section
+
+
+    console.log(array.length);
+    inputToshow = "";
+    //show all input fields and labels that user selected
+    for(i = 2; i < array.length; i++){
+
+        inputToShow = array[i];
+
+        //create an input label and input field depending on array[i] value
+    }
     
-    
+
 
 
 });
