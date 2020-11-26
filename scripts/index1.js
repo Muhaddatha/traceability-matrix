3a80572066e033e05bb54d10130c6ee7ed51494c
hassan977123 <73917792+hassan977123@users.noreply.github.com>
2020-11-26 09:14:52 -0500

    Add files via upload

--- scripts/index1.js
+++ scripts/index1.js
@@ -39,6 +39,7 @@ $(document).ready(function() {
             $("#form-labels-and-input-fields").append('<select id="' + inputIdToShow + '"> <select id="' + inputIdToShow + '"></select> <br>');
             $("#priority").append('"<option value="blank"> </option><option value="low">Low</option> <option value="medium">Medium</option> <option value="high">High</option>"');
 
+            $("#headingsRow").append('<th><strong>Priority</strong></th>')
         }
         else{ //inputs with selects (priority)
 
@@ -51,34 +52,43 @@ $(document).ready(function() {
             // }
             if(inputIdToShow == "req_id"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Requirement ID </label>');
-
+                $("#headingsRow").append('<th><strong>Requirement ID</strong></th>')
             }
             else if(inputIdToShow == "req_descrip"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Requirement Description </label>');
+                $("#headingsRow").append('<th><strong>Requirement Description</strong></th>')
             }
             else if(inputIdToShow == "status"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Status </label>');
+                $("#headingsRow").append('<th><strong>Status</strong></th>')
             }
             else if(inputIdToShow == "class"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Class/Methods/Functions </label>');
+                $("#headingsRow").append('<th><strong>Class/Methods/Functions</strong></th>')
             }
             else if(inputIdToShow == "developer"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Developer(s) </label>');
+                $("#headingsRow").append('<th><strong>Developers(s)</strong></th>')
             }
             else if(inputIdToShow == "use_case_ID"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Use case ID </label>');
+                $("#headingsRow").append('<th><strong>Use Case ID</strong></th>')
             }
             else if(inputIdToShow == "use_case_descrip"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Use case description </label>');
+                $("#headingsRow").append('<th><strong>Use Case Description</strong></th>')
             }
             else if(inputIdToShow == "test_case_ID"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Test case ID </label>');
+                $("#headingsRow").append('<th><strong>Test Case ID</strong></th>')
             }
             else if(inputIdToShow == "test_case_descrip"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Test case description </label>');
+                $("#headingsRow").append('<th><strong>Test Case Description</strong></th>')
             }
             else if(inputIdToShow == "tester"){
                 $("#form-labels-and-input-fields").append('<label for="' + inputIdToShow + '" > Tester </label>');
+                $("#headingsRow").append('<th><strong>Tester</strong></th>')
             }
 
             //Append input field for the user option
@@ -91,6 +101,8 @@ $(document).ready(function() {
         //add row's data to table dt
 
     }
+
+    $("#headingsRow").append('<th><strong>Delete</strong></th>')
 
     //let rowElement = []; //stores all td's of a signle row
   //  let tableRow = [];
@@ -110,6 +122,12 @@ function handleAddRowForm(e) {
       console.log('rowElement:' + rowElement[i-2]);
   }
 
+  $("#theMatrix").append("<tr id='row" + rowID + "'></tr>");
+  for(let i = 0; i < rowElement.length; i++) {
+      $("#row" + rowID).append("<th>" + rowElement[i] + "</th>");
+  }
+    $("#row" + rowID).append("<input type='button' id='delete'" + rowID + "' value='Delete!'>")
+  e.preventDefault();
   //reset form values
   resetForm();
 
@@ -120,7 +138,6 @@ function handleAddRowForm(e) {
   //for (let j = 0; j < tableRow.length; j++) {
   //  console.log('tableRow:' + tableRow[j]);
   //}
-  e.preventDefault();
 }
 
 
