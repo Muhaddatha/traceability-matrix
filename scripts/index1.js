fc875d98380043f982847f618b829ec8a1c851c0
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-11-26 10:56:15 -0500

    changed th to td for row elements

--- scripts/index1.js
+++ scripts/index1.js
@@ -126,7 +126,7 @@ function handleAddRowForm(e) {
 
   $("#theMatrix").append("<tr id='row" + rowID + "'></tr>");
   for(let i = 0; i < rowElement.length; i++) {
-      $("#row" + rowID).append("<th>" + rowElement[i] + "</th>");
+      $("#row" + rowID).append("<td>" + rowElement[i] + "</td>");
   }
     $("#row" + rowID).append("<input type='button' id='delete'" + rowID + "' value='Delete!'>");
   
